1fb1dd50be683ab959008ffc159f56568c699155
Alekh Kumar <alekhkumar777@gmail.com>
2021-09-14 21:17:17 +0530

    Call the backend API to get all the tasks

--- public/todolist.js
+++ public/todolist.js
@@ -4,3 +4,23 @@ let list = document.getElementById("list");
 
 btnAdd.addEventListener("click", addNewTask);
 
+function addNewTask(){
+    let text = inpTask.value;
+    params = {
+        method: "POST", // or 'PUT'
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ task: text }),
+    };
+    let str = '';
+    fetch("/todos/", params)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log("Todo - ", data)
+            for(todo of data){
+                str += "<li>"+todo.task+"</li>"
+            }
+            list.innerHTML = str;
+        });
+}
